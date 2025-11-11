d21e9d3cb70d62af12a5a668a9a8d40b1ae9c097
Dewakar S <dewasreenivas@gmail.com>
2025-11-11 17:54:22 +0530

    changed the url and checked it

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from build_tools import build_tool_from_json
-
+    
 # JSON directly as a dictionary for demonstration
 tool_json = {
-    "tool_name": "product_fetch_tool",
-    "description": "Fetch a single product by ID from FakeStore API.",
-    "args_schema": {
-        "product_id": "int"
-    },
-    "api_url": "https://fakestoreapi.com/products/{product_id}"
+  "tool_name": "product_fetch_tool_dummyjson",
+  "description": "Fetch a single product by ID from DummyJSON API.",
+  "args_schema": {
+    "product_id": "int"
+  },
+  "api_url": "https://dummyjson.com/products/{product_id}"
 }
+
 
 tool = build_tool_from_json(tool_json)
 
